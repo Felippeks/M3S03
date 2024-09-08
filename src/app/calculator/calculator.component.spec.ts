f06811b25152b33bf6205b44dd78a7e5e29d9d5e
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-09-08 17:36:25 -0300

    exercicio 06

--- src/app/calculator/calculator.component.spec.ts
+++ src/app/calculator/calculator.component.spec.ts
@@ -1,23 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CalculatorComponent } from './calculator.component';
+import { CalculatorService } from '../service/calculator.service';
+import { FormsModule } from '@angular/forms';
 
 describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
   let fixture: ComponentFixture<CalculatorComponent>;
+  let calculatorService: CalculatorService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CalculatorComponent]
-    })
-    .compileComponents();
-    
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [CalculatorComponent],
+      providers: [CalculatorService]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CalculatorComponent);
     component = fixture.componentInstance;
+    calculatorService = TestBed.inject(CalculatorService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('deve atualizar o resultado corretamente ao somar dois números', () => {
+    component.num1 = 5;
+    component.num2 = 3;
+    component.somar();
+    expect(component.resultado).toBe(8);
   });
 });
